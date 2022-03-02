3d5ea2a8ac643a5c1cc250e2008d493410cc2344
lucia2207 <ludef15@gmail.com>
2022-03-02 14:58:09 -0300

    avances parte 3

--- crud-kata-frontend/src/App.js
+++ crud-kata-frontend/src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useContext, useEffect, useReducer, useRef, useState } from 'react';
 
-function App() {
+const HOST_API = "http://localhost:8080/api"
+
+const initialState = {
+  list: []
+};
+const Store = createContext(initialState);
+const Form = () => {
+
+  const formRef = useRef(null);
+  const [dispatch] = useContext(Store);
+  const [state, setState] = useState();
+
+  const onAdd = (event) => {
+    event.preventDefault();
+
+    const request = {
+      name: state.name,
+      id: null,
+      isComplete: false,
+    };
+
+    fetch(HOST_API + "/todo", {
+      method: "POST",
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => response.json())
+      .then((todo) => {
+        dispatch({ type: "add-item", item: todo });
+        setState({ name: "" });
+        formRef.current.reset();
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form ref={formRef}>
+      <input type="text" name="name"
+        onChange={(event) =>
+          setState({ ...state, name: event.target.value })}
+      />
+      <button onClick={onAdd}>Agregar</button>
+    </form>
+  );
+
+}
+
+const List = () => { //para listar la info
+  const { state, dispatch } = useContext(Store);
+  
+  useEffect(() => {
+    fetch(HOST_API + "/todos")
+      .then(response => response.json())
+      .then((list) => {
+        dispatch({ type: "update-list", list })
+      })
+  }, [state.list.length, dispatch]);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Nombre</td>
+          <td>¿Esta Completado?</td>
+        </tr>
+      </thead>
+      <tbody>
+        {state.list.map((todo) => {
+          return <tr key={todo.id}>
+            <td>{todo.id}</td>
+            <td>{todo.name}</td>
+            <td>{todo.isComplete}</td>
+          </tr>
+        })}
+      </tbody>
+    </table>
   );
 }
 
-export default App;
+function reducer(state, action) {
+  switch (action.type) {
+    case 'update-list':
+      return { ...state, list: action.list }
+    case 'add-item':
+      const newList = state.list;
+      newList.push(action.item);
+      return { ...state, list: newList }
+    default:
+      return state;
+  }
+}
+
+const StoreProvider = ({ children }) => {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>
+  );
+}
+
+function App() {
+  return (
+    <StoreProvider>
+      <Form />
+      <List />
+    </StoreProvider>
+  );
+}
+
+export default App; 
