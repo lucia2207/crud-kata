4198f6a203dc25ba9283323f99a8a3352708e9c8
lucia2207 <ludef15@gmail.com>
2022-03-02 12:48:13 -0300

    primer commit, base de proyecto

---
+++ crud-kata-backend/src/main/java/co/com/sofka/crudkata/CrudKataApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.crudkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudKataApplication.class, args);
+	}
+
+}
